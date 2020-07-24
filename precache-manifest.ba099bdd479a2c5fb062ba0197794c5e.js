22a75d846526be964ada6b3fb777a6df5d4db111
seelasudheer <seelasudheer4@gmail.com>
2020-07-24 16:46:56 +0530

    Updates

---
+++ precache-manifest.ba099bdd479a2c5fb062ba0197794c5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb9405552e8c42ec2dc2a959ccad1e3",
+    "url": "/seelasudheer/UserTimings/index.html"
+  },
+  {
+    "revision": "8ce62c08acdce2b65815",
+    "url": "/seelasudheer/UserTimings/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7878b377c0bd1711fdcd",
+    "url": "/seelasudheer/UserTimings/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ce62c08acdce2b65815",
+    "url": "/seelasudheer/UserTimings/static/js/2.abbb0bb3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/seelasudheer/UserTimings/static/js/2.abbb0bb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7878b377c0bd1711fdcd",
+    "url": "/seelasudheer/UserTimings/static/js/main.7ecd262d.chunk.js"
+  },
+  {
+    "revision": "3c5f5063b99184589d5b",
+    "url": "/seelasudheer/UserTimings/static/js/runtime-main.7a75d50d.js"
+  }
+]);
